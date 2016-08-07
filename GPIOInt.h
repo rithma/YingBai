2f8ee161ed7e151bc2120dcc2068e656a1cef7d8
rithma <rithers@gmail.com>
2016-08-06 20:16:50 -0700

    Create GPIOInt.h

---
+++ GPIOInt.h
@@ -0,0 +1,10 @@
+//GPIOInt.h  -    header file for the sample project GPIOInt.c
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#define SYSCTL_RCC_XTAL_16MHZ     0x00000540
+#define SYSCTL_RCC_USESYSDIV      0x00400000
+#define SYSCTL_SYSDIV_10          0X04c00000
+#define SYSCTL_RCGC2_GPIOB        0x00000002
+#define SYSCTL_RCGC2_GPIOD        0x00000008
